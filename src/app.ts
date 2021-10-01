8d41dc680690bcfe15c96ab4327dadb1865951f6
JoungSik <tjstlr2010@gmail.com>
2021-10-01 23:52:10 +0900

    Cron 11:45

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import cron from 'node-cron';
+
 import 'chromedriver';
 import { Builder, By } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
@@ -12,80 +14,82 @@ const zooKeyword = ['쥐띠', '소띠', '호랑이띠', '토끼띠', '용띠', '
 const constellKeyword = ['물병자리', '물고기자리', '양자리', '황소자리', '쌍둥이자리', '게자리', '사자자리', '처녀자리', '천칭자리', '전갈자리', '사수자리', '염소자리'];
 const constellDates = ['1월 20일 ~ 2월 18일', '2월 19일 ~ 3월 20일', '3월 21일 ~ 4월 19일', '4월 20일 ~ 5월 20일', '5월 21일 ~ 6월 21일', '6월 22일 ~ 7월 22일', '7월 23일 ~ 8월 22일', '8월 23일 ~ 9월 23일', '9월 24일 ~ 10월 22일', '10월 23일 ~ 11월 22일', '11월 23일 ~ 12월 24일', '12월 25일 ~ 1월 19일'];
 
-(async function crawler() {
-  let data: Array<Fortune> = [];
+cron.schedule('0 45 23 * * *', () => {
+  (async function crawler() {
+    let data: Array<Fortune> = [];
 
-  let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('headless')).build();
+    let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('headless')).build();
 
-  await driver.manage().setTimeouts({ implicit: 10000, pageLoad: 30000, script: 30000 });
+    await driver.manage().setTimeouts({ implicit: 10000, pageLoad: 30000, script: 30000 });
 
-  try {
-    for (let i = 0; i < zooKeyword.length; i++) {
-      await driver.get(`https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${zooKeyword[i]}`);
+    try {
+      for (let i = 0; i < zooKeyword.length; i++) {
+        await driver.get(`https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${zooKeyword[i]}`);
 
-      let fortuneContainer = await driver.findElement(By.id('yearFortune'));
-      let selectFortunes = await driver.findElement(By.id('fortune_birthTab')).findElements(By.css('li > a'));
+        let fortuneContainer = await driver.findElement(By.id('yearFortune'));
+        let selectFortunes = await driver.findElement(By.id('fortune_birthTab')).findElements(By.css('li > a'));
 
-      await selectFortunes[1].click();
+        await selectFortunes[1].click();
 
-      try {
-        await driver.wait(() => {
-          return false;
-        }, 1000);
-      } catch (error) {
+        try {
+          await driver.wait(() => {
+            return false;
+          }, 1000);
+        } catch (error) {
 
+        }
+
+        const fortunes = await fortuneContainer.findElements(By.css('p._cs_fortune_text'));
+        console.log(zooKeyword[i]);
+
+        data.push({
+          name: zooKeyword[i],
+          content: await fortunes[1].getText(),
+          due_date: dayjs().add(1, 'day').format('YYYY-MM-DD'),
+          created_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+          updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
+
+        });
       }
 
-      const fortunes = await fortuneContainer.findElements(By.css('p._cs_fortune_text'));
-      console.log(zooKeyword[i]);
+      for (let i = 0; i < constellKeyword.length; i++) {
+        await driver.get(`https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${constellKeyword[i]}`);
 
-      data.push({
-        name: zooKeyword[i],
-        content: await fortunes[1].getText(),
-        due_date: dayjs().add(1, 'day').format('YYYY-MM-DD'),
-        created_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-        updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
+        let fortuneContainer = await driver.findElement(By.id('yearFortune'));
+        let selectFortunes = await fortuneContainer.findElement(By.css('ul.tab_wrap2._cs_fortune_tab')).findElements(By.css('li > a'));
 
-      });
-    }
+        await selectFortunes[1].click();
 
-    for (let i = 0; i < constellKeyword.length; i++) {
-      await driver.get(`https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${constellKeyword[i]}`);
+        try {
+          await driver.wait(() => {
+            return false;
+          }, 1000);
+        } catch (error) {
 
-      let fortuneContainer = await driver.findElement(By.id('yearFortune'));
-      let selectFortunes = await fortuneContainer.findElement(By.css('ul.tab_wrap2._cs_fortune_tab')).findElements(By.css('li > a'));
+        }
 
-      await selectFortunes[1].click();
+        const fortunes = await fortuneContainer.findElements(By.css('p._cs_fortune_text'));
+        console.log(constellKeyword[i]);
 
-      try {
-        await driver.wait(() => {
-          return false;
-        }, 1000);
-      } catch (error) {
+        data.push({
+          name: constellKeyword[i],
+          content: `${constellDates[i]} ${await fortunes[1].getText()}`,
+          due_date: dayjs().add(1, 'day').format('YYYY-MM-DD'),
+          created_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+          updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
 
+        });
       }
 
-      const fortunes = await fortuneContainer.findElements(By.css('p._cs_fortune_text'));
-      console.log(constellKeyword[i]);
-
-      data.push({
-        name: constellKeyword[i],
-        content: `${constellDates[i]} ${await fortunes[1].getText()}`,
-        due_date: dayjs().add(1, 'day').format('YYYY-MM-DD'),
-        created_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-        updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
-
-      });
+    } finally {
+      await driver.quit();
     }
 
-  } finally {
-    await driver.quit();
-  }
-
-  createConnection().then(async connection => {
-    await connection.createQueryBuilder()
-      .insert()
-      .into(Fortune).values(data)
-      .execute();
-  }).catch(error => console.log(error));
-})();
+    createConnection().then(async connection => {
+      await connection.createQueryBuilder()
+        .insert()
+        .into(Fortune).values(data)
+        .execute();
+    }).catch(error => console.log(error));
+  })();
+});
